a5227e1b1e06008d7984cb6b58096e77a9e748a4
sprabhuraj <prabhuraj@gmail.com>
2023-10-07 23:58:41 -0400

    Add files via upload

---
+++ business_mapping_st.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+st.title("Business Mapping Editor")
+
+# Get the current session context
+session = get_active_session()
+
+column_configuration = {
+    "Oppty_GCoE_Branch_Type": st.column_config.SelectboxColumn(
+                "Oppty GCOE Branch Type",
+                help="Branch Type",
+                width="medium",
+                options=[
+                    "Direct",
+                    "Indirect",
+                    "(none)",
+                ],
+            )
+}
+
+# Create a form
+with st.form("mapping_form"):
+    st.caption("Add or update the mappings in the table below")
+    #  Create a data frame from a table
+    df = session.sql('SELECT Oppty_GCoE_Branch_Type, Oppty_GCoE_Case_Owner_CoEBranch FROM CORE.BUSINESS_MAPPING_TABLE')
+    df.collect()
+    # Convert it into a Pandas data frame
+    pdf = df.to_pandas()
+    edited_pdf = st.data_editor(pdf, column_config=column_configuration, use_container_width=True, num_rows="dynamic")
+    submit_button = st.form_submit_button("Save Data")
+
+if submit_button:
+    try:
+        session.write_pandas(edited_pdf, "BUSINESS_MAPPING_TABLE", overwrite = True)
+        session.sql('GRANT SELECT ON TABLE CORE.BUSINESS_MAPPING_TABLE TO APPLICATION ROLE APP_PUBLIC').collect()
+        st.write('Data saved to table.')
+    except:
+        st.write('Error saving to table.')
